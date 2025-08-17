fefd3b368a644c645d15e306020af5bc2d59f7c5
Toni <tonniicode@gmail.com>
2025-08-17 19:19:42 +0800

    add navbar for mobile screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,17 @@
-const hamburberBtn = document.querySelector(".hamburger-menu");
-const displayNav = document.querySelector(".navbar");
-const btnClose = document.createElement("button");
+const hamburgerBtn = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("nav");
+const span = document.querySelectorAll(".hamburger-menu span");
 
-hamburberBtn.addEventListener("click", () => {
-  if ((hamburberBtn.style.display = "block" === "block")) {
-    hamburberBtn.style.display = "none";
-    displayNav.style.display = "block";
-    btnClose.innerHTML = "X";
-    btnClose.style.position = "absolute";
-    btnClose.style.top = "55px";
-    btnClose.style.right = "5px";
-    btnClose.style.padding = "8px 10px";
-    btnClose.style.cursor = "pointer";
-    btnClose.style.outLine = "none";
-    btnClose.style.border = "none";
-    btnClose.style.backgroundColor = "rgb(43, 158, 43)";
-    btnClose.style.color = "#eee";
-    btnClose.style.borderRadius = "5px";
-    displayNav.appendChild(btnClose);
-  }
-});
-
-btnClose.addEventListener("mouseover", () => {
-  btnClose.style.backgroundColor = "rgb(43 158 43 / 85%)";
-});
-
-btnClose.addEventListener("mouseout", () => {
-  btnClose.style.backgroundColor = "rgb(43, 158, 43)";
-});
-
-btnClose.addEventListener("click", () => {
-  hamburberBtn.style.display = "block";
-  displayNav.style.display = "none";
+hamburgerBtn.addEventListener("click", () => {
+  span.forEach((elem) => {
+    if (elem.getAttribute("class") === "hamburger-off") {
+      elem.setAttribute("class", "hamburger-on");
+      navbar.style.right = "0";
+      navbar.style.transition = "0.5s";
+    } else {
+      elem.setAttribute("class", "hamburger-off");
+      navbar.style.right = "-450px";
+      navbar.style.transition = "0.5s";
+    }
+  })
 });
