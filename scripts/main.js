6df141ef673d0d3376d9b8633bfae96b5283a755
TonDev <tonniicode@gmail.com>
2026-09-13 11:45:37 +0700

    create function hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,27 @@
-const navbar = document.querySelector("nav");
-const span = document.querySelectorAll(".hamburger-menu span");
-const hamburgerBtn = document.querySelector(".hamburger-menu");
-const lightDarkBtn = document.querySelector("#light-dark");
+const hamburgerBtn = document.getElementById("hamburger-btn");
 
-function navbarMobile() {
-  span.forEach((elem) => {
-    let getClassSpan = elem.getAttribute("class");
+let isMenuHamburger = false;
 
-    if (getClassSpan === "hamburger-off") {
-      elem.setAttribute("class", "hamburger-on");
-      navbar.style.right = "0";
-      navbar.style.transition = "0.5s";
-    } else {
-      elem.setAttribute("class", "hamburger-off");
-      navbar.style.right = "-1000px";
-      navbar.style.transition = "0.5s";
-    }
-  });
-}
+hamburgerBtn.addEventListener("click", () => {
+  const navContent = document.querySelector("ul");
+  const contactBtn = document.querySelector(".contact-btn");
+  const linesTop = document.querySelector(".hamburger-btn .top");
+  const linesMiddle = document.querySelector(".hamburger-btn .middle");
+  const linesBottom = document.querySelector(".hamburger-btn .bottom");
 
-function lightDarkMode() {
-  const body = document.querySelector("body");
-  const header = document.querySelector("header");
-  const nav = document.querySelector("nav");
-  const circle = document.querySelector(".circle");
-  const anchor = document.querySelectorAll("nav a");
-
-  let classList = lightDarkBtn.classList;
-  let checkList = classList.contains("light");
-
-  if (checkList) {
-    body.style.backgroundColor = "#333";
-    header.style.backgroundColor = "#333";
-    nav.style.backgroundColor = "#333";
-    circle.style.marginLeft = "20px";
-    anchor.forEach((elem) => (elem.style.color = "#fff"));
-
-    body.style.transition = "background-color 0.4s";
-    header.style.transition = "background-color 0.4s";
-    nav.style.transition = "background-color 0.4s";
-    circle.style.transition = "margin-left 0.3s";
-
-    lightDarkBtn.classList.replace("light", "dark");
+  if (isMenuHamburger === false) {
+    isMenuHamburger = true;
+    navContent.classList.add("active");
+    contactBtn.classList.add("active");
+    linesTop.classList.add("active");
+    linesMiddle.classList.add("active");
+    linesBottom.classList.add("active");
   } else {
-    body.style.backgroundColor = "#fff";
-    header.style.backgroundColor = "#fff";
-    nav.style.backgroundColor = "#fff";
-    anchor.forEach((elem) => (elem.style.color = "#333"));
-    circle.style.marginLeft = "2.5px";
-    lightDarkBtn.classList.replace("dark", "light");
+    isMenuHamburger = false;
+    navContent.classList.remove("active");
+    contactBtn.classList.remove("active");
+    linesTop.classList.remove("active");
+    linesMiddle.classList.remove("active");
+    linesBottom.classList.remove("active");
   }
-}
-
-hamburgerBtn.addEventListener("click", navbarMobile);
-lightDarkBtn.addEventListener("click", lightDarkMode);
+});
